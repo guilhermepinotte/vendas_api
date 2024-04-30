fda6f171c57aa5d5ac23d7543ca8066f106c279d
Guilherme Nicchio Pinotte <guilherme.pinotte@serpro.gov.br>
2024-04-30 16:56:07 -0300

    Cadastrar e Detalhar clientes funcinando

---
+++ src/main/java/io/github/guilhermepinotte/vendas/domain/cliente/DadosDetalhamentoCliente.java
@@ -0,0 +1,15 @@
+package io.github.guilhermepinotte.vendas.domain.cliente;
+
+import io.github.guilhermepinotte.vendas.domain.endereco.Endereco;
+
+public record DadosDetalhamentoCliente(
+    Long id,
+    String nome,
+    String email,
+    TipoCliente tipo,
+    Endereco endereco
+) {
+    public DadosDetalhamentoCliente(Cliente c){
+        this(c.getId(),c.getNome(),c.getEmail(),c.getTipo(),c.getEndereco());
+    }
+}
